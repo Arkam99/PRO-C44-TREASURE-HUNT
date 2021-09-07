e868096353c20937cc6e21fe0d08eb3000b047cb
Arkam99 <84503127+Arkam99@users.noreply.github.com>
2021-09-07 21:54:15 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+var bg,bg2,form,system,code,security;
+var score=0;
+
+function preload() {
+  bg = loadImage("aladdin_cave.jpg");
+}
+
+function setup() {
+  createCanvas(1000,500);
+  system = new System()
+  security = new Security()
+}
+
+function draw() {
+  background(bg);
+  clues();
+  security.display();
+
+ 
+  // Add code to display score in the middle of the screen
+  text("S C O R E / "+score,500,50);
+
+  // Add code to display the end screen
+  if(score === 3){
+    clear();
+    background(bg2);
+    fill("black");
+    textSize(40);
+    text("TREASURE_UNLOCKED",250,200);
+  }
+
+  drawSprites()
+}
